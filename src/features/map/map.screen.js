4386b5cee14e90e97de6d0bdff0b59ecc5ba181e
Ganesh <ganeshmurugan0123@gmail.com>
2022-04-30 00:17:13 +0530

    map

--- src/features/map/map.screen.js
+++ src/features/map/map.screen.js
@@ -1,4 +1,52 @@
 import React from "react";
-import MapView from "react-native-maps";
+import { Dimensions, Text, View } from "react-native";
+import MapView, { Callout, Circle, Marker } from "react-native-maps";
+import styled from "styled-components/native";
+import { SafeArea } from "../../components/utility/safe-area.component";
+import { Foundation } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
 
-export const MapScreen = () => <MapView style={{ height: "100%" }} />
+const StyledMapView = styled(MapView)`
+  width: ${Math.floor(Dimensions.get("window").width)}px;
+  height: ${Math.floor(Dimensions.get("window").height) - 40}px;
+`;
+const MapContainer = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const MapScreen = () => (
+  <SafeArea>
+    <MapContainer>
+      <StyledMapView
+        region={{
+          latitude: 8.69659071711335,
+          longitude: 77.74215476000238,
+          latitudeDelta: 0.0806,
+          longitudeDelta: 0.0411,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: 8.69659071711335,
+            longitude: 77.74215476000238,
+          }}
+          key={Marker.key}
+        >
+          <Callout>
+            <Text>home</Text>
+          </Callout>
+        </Marker>
+        <Circle
+          center={{
+            latitude: 8.69659071711335,
+            longitude: 77.74215476000238,
+          }}
+          radius={2000}
+        />
+      </StyledMapView>
+    </MapContainer>
+  </SafeArea>
+);
